4ea541c92b474ee286128ea02dca4e91c79de6bf
Jumpad <bogdanmasnij67@gmail.com>
2024-03-19 00:28:55 +0200

    fix: add function name to Rectangle function

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -177,7 +177,7 @@ function sellTickets(queue) {
 function Rectangle(width, height) {
   this.width = width;
   this.height = height;
-  this.getArea = function () {
+  this.getArea = function pooki() {
     return this.width * this.height;
   };
 }
